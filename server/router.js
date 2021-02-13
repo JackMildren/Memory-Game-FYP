2c152f9891cc1fc510676bdadb50f1d34d5ea89b
Jack Mildren <jmildren1@gmail.com>
2021-02-13 05:38:28 +0000

    Minor edit to redirect to homepage for any invalid url extension

--- server/router.js
+++ server/router.js
@@ -9,8 +9,12 @@ router.get('/', function (req, res) {
     res.sendFile("index.html", {root: path.join(__dirname, "../public/views")});
 });
 
+router.get('/user', function (req, res) {
+    res.sendFile("user.html", {root: path.join(__dirname, "../public/views")});
+});
+
 router.get('/*', function (req, res) {
-    res.sendFile("index.html", {root: path.join(__dirname, "../public/views")});
+    res.redirect('/');
 });
 
 module.exports = router;
